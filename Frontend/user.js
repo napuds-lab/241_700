93298b82ed56e3d2c57a15bc97732f89c195b00f
napuds-lab <napud.s@ku.th>
2026-03-14 11:29:56 +0700

    Update user.js

--- Frontend/user.js
+++ Frontend/user.js
@@ -10,11 +10,14 @@ const loadData = async () => {
     let htmlData = '<div>';
     for (let i = 0; i < response.data.length; i++) {
         let user = response.data[i];
-        htmlData += ` <div>
-        ${user.id} ${user.firstname} ${user.lastname}
-        <button>Edit</button>
-        <button class='delete' data-id='${user.id}'>Delete</button>
-        </div>`
+        let firstname = user.firstname || user.first_name || '';
+        let lastname = user.lastname || user.last_name || '';
+        let id = user.id || user.ID || '';
+        htmlData += `<div>
+        ${id} ${firstname} ${lastname}
+        <a href="index.html?id=${id}"><button>Edit</button></a>
+        <button class='delete' data-id='${id}'>Delete</button>
+    </div>`;
     }
     htmlData += '</div>';
     userDOM.innerHTML = htmlData;
